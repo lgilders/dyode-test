9ed2593f99284f70b7391cab1834d7da117ba5ce
Lori Gildersleeve <lgildersleeve@gmail.com>
2020-07-29 00:06:25 -0400

    Initial commit of new theme with gulp setup, initial styles, and global bar added.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(rename('styles.css'))
+        .pipe(replace('"{{', '{{'))
+        .pipe(replace('}}"', '}}'))
+        .pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('default', function() {
+    gulp.watch(['./sass/**/*.scss'], gulp.series(['sass']));
+});
