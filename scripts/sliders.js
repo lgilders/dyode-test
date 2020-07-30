47f94738762a38028d99a1a5a5d64de3e968912c
Lori Gildersleeve <lgildersleeve@gmail.com>
2020-07-30 18:50:19 -0400

    Added jquery and slick slider to theme. Added instagram section and styling.

---
+++ scripts/sliders.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    var $instagram = $('#instagram-wrapper'); // Instagram section
+
+    initCarousel();
+
+    $(window).on('orientationchange resize', function() {
+        initCarousel();
+    });
+
+    function initCarousel() {
+        $instagram.each(function() { 
+            if (!$(this).hasClass('slick-initialized')) {
+                $(this).slick({
+                    dots: false,
+                    arrows: false,
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    mobileFirst: true,
+                    adaptiveHeight: true,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: "unslick"
+                        }
+                    ]
+                });
+            }
+        })
+    }
+});
